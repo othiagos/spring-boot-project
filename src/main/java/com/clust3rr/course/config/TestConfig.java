4dd71beca72ab1066909cebe460f0b466b21b0ec
clust3rr <thaigosilvasantos.dev@gmail.com>
2020-07-20 18:58:14 -0300

    Payment, one-to-one association

--- src/main/java/com/clust3rr/course/config/TestConfig.java
+++ src/main/java/com/clust3rr/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.clust3rr.course.entities.Category;
 import com.clust3rr.course.entities.Order;
 import com.clust3rr.course.entities.OrderItem;
+import com.clust3rr.course.entities.Payment;
 import com.clust3rr.course.entities.Product;
 import com.clust3rr.course.entities.User;
 import com.clust3rr.course.entities.enums.OrderStatus;
@@ -77,6 +78,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRespository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 
